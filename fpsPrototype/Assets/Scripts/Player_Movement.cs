0df53963caa47666c572db4c9ac5562316f82f74
Xury Greer <Xury.Greer@Gmail.com>
2019-10-11 01:00:58 -0700

    Added Quake movement code to the project for reference (not implemented) Added ability to rocket jump while grounded Fixed a bug that caused two or more dust clouds to spawn during a ground pound.

--- fpsPrototype/Assets/Scripts/Player_Movement.cs
+++ fpsPrototype/Assets/Scripts/Player_Movement.cs
@@ -6,9 +6,11 @@ public class Player_Movement : MonoBehaviour
 {
 	// User Preference Head Bob
 	public bool userPreference_EnableLandingShake = true;
+	public bool userPreference_autoAddJumpForceToGroundedRocketJump = true;
 	
 	// Object References
-	public GameObject groundPoundParticles;
+	public GameObject groundPoundParticles; // gets reference to the particles to spawn
+	private GameObject gpParticles_GameObject; // stores the instance of the particles
 	private GameObject camOffset;
 	private GameObject firstPersonCam;
 	private Rigidbody playerRB;
@@ -20,6 +22,17 @@ public class Player_Movement : MonoBehaviour
 	// Jumping Variables
 	[SerializeField] private float jumpForceMultiplier =  400.0f;
 	
+	
+	
+	private const float rjBlast_Range = 3.0f;
+	private const float rjBlast_Power = 550.0f;
+	private Vector3 rjBlast_Epicenter; // The origin of the rocket jump blast radius.
+	private const float rjBlast_Radius = 5.0f;
+	private const float rjBlast_UpwardForce = 0.0f;
+	private const float minRocketJumpCameraAngle = 45.0f;
+	
+	public float cameraAngle = 0.0f;
+	
 	private float impactVelocity = 0.0f;
 	public float minGroundPoundVelocity = 8.0f;
 	public float groundPound_Multiplier = 25.0f;
@@ -27,12 +40,6 @@ public class Player_Movement : MonoBehaviour
 	// Rocket Jumping Variables
 	private int rjBlast_NumSinceGrounded = 0;
 	[SerializeField] private const int rjBlast_NumLimit = 2;
-	
-	[SerializeField] private const float rjBlast_Range = 3.0f;
-	[SerializeField] private const float rjBlast_Power = 550.0f;
-	private Vector3 rjBlast_Epicenter;
-	[SerializeField] private const float rjBlast_Radius = 5.0f;
-	[SerializeField] private const float rjBlast_UpwardForce = 0.5f;
 	
 	[SerializeField] private float movement_SpeedMultiplier = 50.0f;
 	[SerializeField] private const float movement_MaxSpeed = 20.0f;
@@ -66,6 +73,8 @@ public class Player_Movement : MonoBehaviour
 
     void Update()
     {		
+		cameraAngle = firstPersonCam.transform.eulerAngles.x;
+		
 		GetInput_LateralMovement();
 		GetInput_Mouse();
 		LookUpDown();
@@ -74,7 +83,7 @@ public class Player_Movement : MonoBehaviour
 		else firstPersonArms_Animator.SetBool("fire", false);
 		
 		
-		if (Input.GetButtonDown("Fire2")) RocketJump();
+		if (Input.GetButtonDown("Fire2")) RocketJumpCheck();
 		if (Input.GetButtonDown("Jump") && IsGrounded()) Jump();
 		if (Input.GetButton("Crouch") &&  !IsGrounded()) AccelerateDown();
     }
@@ -168,22 +177,40 @@ public class Player_Movement : MonoBehaviour
 		playerRB.AddRelativeForce(Vector3.up * jumpForceMultiplier, ForceMode.Impulse);
 	}
 	
+	// Called via player input, Checks if the conditions to be able to rocket jump are met,
+	// if so, calls the rocket jump method that performs the rocket jump.
+	private void RocketJumpCheck()
+	{	
+		if (rjBlast_NumSinceGrounded < rjBlast_NumLimit) // Don't bother with the rest of the check if the player has used up all of his rocket jumps.
+		{
+			// Determine where the rocket jump blast's center point should be.
+			if (Physics.Raycast(firstPersonCam.transform.position, firstPersonCam.transform.forward, out RaycastHit hit, rjBlast_Range, LayerMask.NameToLayer("Player"))) rjBlast_Epicenter = hit.point;
+			else rjBlast_Epicenter = firstPersonCam.transform.position + (firstPersonCam.transform.forward * rjBlast_Range);
+		
+			firstPersonArms_Animator.Play("FirstPersonArms_Blast", 1, 0.25f); // Play the blast animation.
+			BlastForce(rjBlast_Power, rjBlast_Epicenter, rjBlast_Radius, rjBlast_UpwardForce); // Add the blast force to affect other objects.
+			
+			bool blastOffAngleOK = false;
+			if (firstPersonCam.transform.localEulerAngles.x <= 90.0f && firstPersonCam.transform.localEulerAngles.x >= 45.0f) blastOffAngleOK = true;
+			
+			if (IsGrounded())
+			{
+				if (blastOffAngleOK)
+				{
+					// Force the player into the air (as if he jumped) before applying the rocket jump to get a compounding force.
+					if (userPreference_autoAddJumpForceToGroundedRocketJump) StartCoroutine(JumpThenRocketJump());
+					else RocketJump(); // Rocket jump wihtout jumping first.
+				}
+			}
+			else RocketJump();
+		}
+	}
+
+	// Called via the Rocket Jump Check method, this actually performs the rocket jump.
 	private void RocketJump()
 	{
-		if (rjBlast_NumSinceGrounded < rjBlast_NumLimit)
-		{
-			if(Physics.Raycast(firstPersonCam.transform.position, firstPersonCam.transform.forward, out RaycastHit hit, rjBlast_Range, LayerMask.NameToLayer("Player"))) rjBlast_Epicenter = hit.point;
-			else rjBlast_Epicenter = firstPersonCam.transform.position + (firstPersonCam.transform.forward * rjBlast_Range);
-			
-			BlastForce(rjBlast_Power, rjBlast_Epicenter, rjBlast_Radius, rjBlast_UpwardForce);
-			firstPersonArms_Animator.Play("FirstPersonArms_Blast", 1, 0.25f);
-			
-			if (!IsGrounded())
-			{
-				playerRB.AddExplosionForce(rjBlast_Power, rjBlast_Epicenter, rjBlast_Radius, 0.0f, ForceMode.Impulse);
-				rjBlast_NumSinceGrounded += 1;
-			}
-		}
+		playerRB.AddExplosionForce(rjBlast_Power, rjBlast_Epicenter, rjBlast_Radius, rjBlast_UpwardForce, ForceMode.Impulse);
+		rjBlast_NumSinceGrounded += 1;
 	}
 	
 	private void AccelerateDown()
@@ -213,8 +240,11 @@ public class Player_Movement : MonoBehaviour
 			BlastForce(gpBlast_Power, playerRB.position, gpBlast_Radius, gpBlast_UpwardForce); // Apply a blast around the landing
 			
 			// Particle System
-			float gpParticle_Duration = camShake_Duration * 3.0f;
-			GameObject gpParticles_GameObject = Instantiate(groundPoundParticles, playerRB.position, Quaternion.identity) as GameObject;
+			
+			if (gpParticles_GameObject != null) Destroy(gpParticles_GameObject); // to prevent multiple particle systems from spawning when clipping the corner of rounded objects.
+	
+			float gpParticle_Duration = camShake_Duration * 3.0f;			
+			gpParticles_GameObject = Instantiate(groundPoundParticles, playerRB.position, Quaternion.identity) as GameObject;
 			ParticleSystem.MainModule gpParticles_MainModule = gpParticles_GameObject.GetComponent<ParticleSystem>().main;
 			
 			gpParticles_MainModule.startSpeed = Mathf.Clamp(impactVelocity, 5.0f, 50.0f);
@@ -238,6 +268,15 @@ public class Player_Movement : MonoBehaviour
 			// If the object has a rigidbody component and it is not the player, add the blast force!
 			if (rb != null && rb != playerRB) rb.AddExplosionForce(blast_Power, blast_Epicenter, blast_Radius, blast_UpwardForce, ForceMode.Impulse);
 		}
+	}
+	
+	// This method includes a tiny delay to ensure that the counter for mid-air rocket jumps isn't thrown off
+	// AND so that the two forces aren't applied inhumanly fast resulting in a higher jump than is otherwise humanly reflexivly possible.
+	public IEnumerator JumpThenRocketJump()
+	{
+		Jump();
+		yield return new WaitForSeconds(0.1f);
+		RocketJump();
 	}
 	
 	public IEnumerator ParticleTimer (float seconds, GameObject particleObjectToDestroy)
