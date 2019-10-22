5d9e141b0994b602edbe42d72a3267184348164c
Xury Greer <Xury.Greer@Gmail.com>
2019-10-21 20:29:15 -0700

    Added better smoothing when switching gravity sources.

--- fpsPrototype/Assets/Scripts/Gravity_Source.cs
+++ fpsPrototype/Assets/Scripts/Gravity_Source.cs
@@ -20,14 +20,34 @@ public class Gravity_Source : MonoBehaviour
 	}
 	
 	public void AttractObject(Transform attractedObject, float blend)
-	{
+	{		
 		// Rotate the attracted object so that its downward direction faces the gravity source
 		Vector3 gravityVector = GetGravityVector(attractedObject);
 		
+		// Calculate how far the object has to rotate.
+		float rotationDegrees = Vector3.Angle(-attractedObject.up, gravityVector.normalized);
+		
+		float rotationStep = Mathf.InverseLerp(0.0f, 180.0f, rotationDegrees); // Convert the angles 0-180 to a 0-1 mapping.
+		
+		// 0.0f doesn't rotate at all, 1.0f rotates instantly.
+		// 0.03f is fast, for things will small angle diferences, 0.0025f is slow for things with large angle differences.
+		rotationStep = Mathf.Lerp(0.03f, 0.0025f, rotationStep); // Convert the angles 0-180 to a 0-1 mapping.
+		
+		
+		
+		/*
+		rotationStep = Vector3.Distance(attractedObject.position, transform.position);
+		rotationStep = Mathf.InverseLerp(0.0f, 50.0f, rotationStep); // Convert the angles 0-180 to a 0-1 mapping.
+		rotationStep = Mathf.Lerp(0.03f, 0.0025f, rotationStep); // Convert the angles 0-180 to a 0-1 mapping.
+		*/
+		
+		
+		
 		// Set the target rotation (aim at gravity source)
 		Quaternion targetRotation = Quaternion.FromToRotation(-attractedObject.up, gravityVector.normalized) * attractedObject.rotation;
-				
-		attractedObject.rotation = Quaternion.Slerp(attractedObject.rotation, targetRotation, blend);
+		
+		// Rotate towards the target.
+		attractedObject.rotation = Quaternion.Slerp(attractedObject.rotation, targetRotation, rotationStep);
 		
 		// Add force to the attracted object to simulate gravity toward the gravity source.
 		attractedObject.gameObject.GetComponent<Rigidbody>().AddForce(gravityVector, ForceMode.Acceleration);
